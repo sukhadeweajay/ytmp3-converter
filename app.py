79f01a28afd33b032c6e6f5f56b4b59da896dd11
sukhadeweajay <hub79792@gmail.com>
2025-04-19 15:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, send_file
 import yt_dlp
-import os
-
-app = Flask(__name__)
-
+import import os
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 @app.route('/')
 def index():
     return """
